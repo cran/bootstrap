886007ae1c51e7166c0aef2791035e056403690b
Friedrich Leisch <Friedrich.Leisch@ci.tuwien.ac.at>
2002-05-20 00:00:00 +0000

    version 1.0-10

--- R/abcnon.R
+++ R/abcnon.R
@@ -30,12 +30,12 @@ call <- match.call()
 	za <- Z/(1 - a * Z)^2
 	stan <- t0 + sighat * qnorm(alpha)
 	abc <- seq(alpha)
-pp_matrix(0,nrow=n,ncol=length(alpha))
+        pp <- matrix(0,nrow=n,ncol=length(alpha))
 	for(i in seq(alpha)) {abc[i] <- tt(P0 + za[i] * delta,x)
-              pp[,i]_P0 + za[i] * delta
+                              pp[,i] <- P0 + za[i] * delta
        }
 	limits <- cbind(alpha, abc, stan)
-        dimnames(limits)[[2]]_c("alpha", "abc", "stan")
+        dimnames(limits)[[2]] <- c("alpha", "abc", "stan")
 #output in list form.....................................................
         return(limits=limits, stats=list(t0=t0,sighat=sighat,bhat=bhat), 
 constants=list(a=a,z0=z0,cq=cq), tt.inf=t., pp=pp, call=call)
